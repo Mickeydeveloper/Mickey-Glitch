a58fe1d3d396c2f6227ab820dcaaa666f60d0e8a
Mickeydeveloper <mickdenzel24@gmail.com>
2025-12-20 15:25:53 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "𝙼𝚒𝚌𝚔𝚎𝚢 𝙶𝚕𝚒𝚝𝚌𝚑",
   botOwner: 'Mickey', // Your name
-  ownerNumber: '255612130873', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '255615944741', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
