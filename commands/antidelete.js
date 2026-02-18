763589d7357adf28bfee1130e04b5780bb978418
Mickeydeveloper <mickdenzel24@gmail.com>
2026-02-18 16:51:13 +0300

    Fix time formatting options in antidelete.js

--- commands/antidelete.js
+++ commands/antidelete.js
@@ -228,8 +228,7 @@ async function handleMessageRevocation(sock, revocationMessage) {
 
         const time = new Date().toLocaleString('en-US', {
             timeZone: 'Africa/Dar_es_Salaam',
-            hour12: true, hour: '2-digit', minute: '2-digit', second: '2-digit','
-            day: '2-digit', month: '2-digit', year: 'numeric'
+            
         });
 
         // Enhanced report text
