06ca1f22b186669ca61c05e72093172ae82a2186
Mickeydeveloper <mickdenzel24@gmail.com>
2026-02-27 16:48:17 +0300

    Update index.js to implement cleanup for tmp and temp folders

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-setInterval(() => { 
-    const cleanupFolders = ['tmp', 'temp']; // Removed 'baileys'
-    cleanupFolders.forEach(folder => {
-        // Logic to delete folder
+const fs = require('fs');
+const path = require('path');
+const nodemailer = require('nodemailer');
+
+const cleanupDirectories = () => {
+    const directoriesToDelete = ['tmp', 'temp'];
+    const projectPath = __dirname;  // assuming the index.js is in the project root
+
+    directoriesToDelete.forEach(dir => {
+        const dirPath = path.join(projectPath, dir);
+        if (fs.existsSync(dirPath)) {
+            fs.rmdirSync(dirPath, { recursive: true });
+            console.log(`Deleted directory: ${dirPath}`);
+            // Send bot notification here
+            // For example, you could use nodemailer or any other notification service
+        } else {
+            console.log(`Directory not found: ${dirPath}`);
+        }
     });
-}, 120000); // 120000 ms = 2 minutes
+};
+
+setInterval(cleanupDirectories, 120000);  // 120000 milliseconds = 2 minutes
+
+console.log('Cleanup function initiated.');
