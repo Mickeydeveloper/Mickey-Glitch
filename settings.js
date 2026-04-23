8d65f450e17bf717400e566b1717ff7bd51ce849
Mickeydeveloper <mickdenzel24@gmail.com>
2026-04-23 16:41:49 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,21 @@
+const settings = {
+  packname: '𝙼𝚒𝚌𝚔𝚎𝚢 𝙶𝚕𝚒𝚝𝚌𝚑™',
+  author: '‎',
+  botName: "𝙼𝚒𝚌𝚔𝚎𝚢 𝙶𝚕𝚒𝚝𝚌𝚑",
+  botOwner: 'Mickey', // Your name
+  ownerNumber: '255612130873', //Set your number here without + symbol, just add country code & number without any space
+  
+  // Auto Status Sync Settings
+  syncTarget: '255612130873', // Target number for status sync (set to owner number)
+  syncDelay: 6, // Low number delay in seconds between syncs
+  
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.5",
+  updateZipUrl: "https://github.com/Mickeydeveloper/Mickey-Glitch/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
