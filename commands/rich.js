5c557bcb2d39608009a18cb8d1671e6757e72bb0
Mickeymozy <157571260+Mickeymozy@users.noreply.github.com>
2026-08-09 12:14:46 +0300

    Update rich.js

--- commands/rich.js
+++ commands/rich.js
@@ -27,7 +27,7 @@ const richCommand = async (sock, chatId, msg, args) => {
                 metadata: {
                   latex_expression: 'NIXCODE',
                   latex_image: {
-                    url: 'https://d.uguu.se/vJjZyByw.jpg',
+                    url: 'https://d.uguu.se/DGpDqiQG.jpg',
                     width: 1279,
                     height: 825,
                   },
